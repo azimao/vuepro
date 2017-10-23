95af1c087a6cc02bcb1717ca8eb7dc341b63ddf6
azimao <599287041@qq.com>
2017-10-23 23:47:25 +0800

    详情上传

---
+++ src/components/news-detail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="news-list">
+        <div class="row">
+            <div class="page-header">
+                <h2> {{newsdetail.title}}  <small>{{newsdetail.pubtime}}</small></h2>
+                <p>
+                {{newsdetail.desc}}
+                </p>
+            </div>
+
+        </div>
+    </div>
+</template>
+<style>
+     
+</style>
+<script>
+
+    export default{
+        created()
+        {
+            this.$http.get("http://localhost:9090/news.php?newsid="+this.$route.params.newsid)
+                    .then(function(res){
+                        this.newsdetail=res.body;
+                    },function(rs){})
+        },
+        data(){
+            return{
+                newsdetail:{}
+            }
+        }
+    }
+</script>
